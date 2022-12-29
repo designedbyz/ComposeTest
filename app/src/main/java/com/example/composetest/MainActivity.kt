413dee10d995ef940c02df7a38e6807cf01e6f82
Andy Zolyak <andy.zolyak@mavenclinic.com>
2022-12-29 17:03:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.composetest
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composetest.ui.theme.ComposeTestTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var sampleText = remember {mutableStateOf("default text")}
+            ComposeTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                    TextField(
+                        value = sampleText.value,
+                        onValueChange = {
+                            Log.d("LOGLOGLOG", "new value is $it")
+//                            sampleText.value = it
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeTestTheme {
+        Greeting("Android")
+    }
+}
